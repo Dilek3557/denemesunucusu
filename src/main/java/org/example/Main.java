b1c262610bd87d74bfa154ea8202544c2c117292
Dilek Karaca <dilek.karaca@mf.karaelmas.edu.tr>
2026-03-29 02:35:34 +0300

    ilk commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController // Bu, internetten gelen isteklere cevap vereceğim demek
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/selam") // Tarayıcıya veya Postman'e bunu yazınca çalışacak
+    public String selamVer() {
+        return "İnşaat Mühendisi Görev Takip Sistemine Hoş Geldiniz!";
+    }
+}
